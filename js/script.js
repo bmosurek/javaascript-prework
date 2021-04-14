bd2d5ac936ba807750ff9b10b186d58296e800cf
Bartek Mosurek <bartlomiej.mosurek@gmail.com>
2021-04-14 20:37:18 +1000

    Change script.js: add game result

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ if (randomNumber == 1) {
   computerMove = "kamień";
 } else if (randomNumber == 2) {
   computerMove = "papier";
-} else {
+} else if (randomNumber == 3) {
   computerMove = "nożyce";
 }
 
@@ -22,10 +22,28 @@ let playerMove = "nieznany ruch";
 
 if (playerInput == "1") {
   playerMove = "kamień";
-} else if (playerInput == 2) {
+} else if (playerInput == "2") {
   playerMove = "papier";
-} else {
+} else if (playerInput == "3") {
   playerMove = "nożyce";
 }
 
 printMessage("Twój ruch to: " + playerMove);
+
+if (computerMove == "kamień" && playerMove == "papier") {
+  printMessage("Ty wygrywasz!");
+} else if (computerMove == "papier" && playerMove == "nożyce") {
+  printMessage("Ty wygrywasz!");
+} else if (computerMove == "nożyce" && playerMove == "kamień") {
+  printMessage("Ty wygrywasz!");
+} else if (computerMove == playerMove) {
+  printMessage("Remis!");
+} else if (computerMove == "kamień" && playerMove == "nożyce") {
+  printMessage("Ty przegrywasz!");
+} else if (computerMove == "papier" && playerMove == "kamień") {
+  printMessage("Ty przegrywasz!");
+} else if (computerMove == "nożyce" && playerMove == "papier") {
+  printMessage("Ty przegrywasz!");
+} else {
+  printMessage("Wykonałeś niedozwolony ruch!");
+}
