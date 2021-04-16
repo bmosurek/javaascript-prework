fb8b5c4a4104f8eae1425e16545cbc07fb950228
Bartlomiej Mosurek <bartlomiej.mosurek@gmail.com>
2021-04-17 05:26:05 +1000

    Fix script.js: displayResults

--- js/script.js
+++ js/script.js
@@ -38,14 +38,6 @@ console.log("Wylosowana liczba to: " + randomNumber);
 
 let computerMove = getMoveName(randomNumber);
 
-/*if (randomNumber == 1) {
-  computerMove = "kamień";
-} else if (randomNumber == 2) {
-  computerMove = "papier";
-} else if (randomNumber == 3) {
-  computerMove = "nożyce";
-}
-*/
 printMessage("Mój ruch to: " + computerMove);
 
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
@@ -64,26 +56,4 @@ if (playerInput == "1") {
 
 printMessage("Twój ruch to: " + playerMove);
 
-/*if (computerMove == "kamień" && playerMove == "papier") {
-  printMessage("Ty wygrywasz!");
-} else if (computerMove == "papier" && playerMove == "nożyce") {
-  printMessage("Ty wygrywasz!");
-} else if (computerMove == "nożyce" && playerMove == "kamień") {
-  printMessage("Ty wygrywasz!");
-} else if (computerMove == playerMove) {
-  printMessage("Remis!");
-} else if (computerMove == "kamień" && playerMove == "nożyce") {
-  printMessage("Ty przegrywasz!");
-} else if (computerMove == "papier" && playerMove == "kamień") {
-  printMessage("Ty przegrywasz!");
-} else if (computerMove == "nożyce" && playerMove == "papier") {
-  printMessage("Ty przegrywasz!");
-} else {
-  printMessage("Wykonałeś niedozwolony ruch!");
-}*/
-
-console.log("moves:", argComputerMove, argPlayerMove);
-
-console.log(displayResult(argComputerMove, argPlayerMove));
-
-displayResult(argComputerMove, argComputerMove);
+displayResult(computerMove, playerMove);
